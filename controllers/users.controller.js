0b8029b5806ede1217b71857561bbad2f2d50dd1
SR Rony <146630569+SR-Rony@users.noreply.github.com>
2024-01-24 22:36:17 +0600

    users post request done

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,7 +1,21 @@
-const users = require("../models/users.model")
+let users = require("../models/users.model")
+const {v4:uuidv4} = require("uuid")
 
+
+// users get requist
 const getAllUsers = (req,res)=>{
     res.status(200).json(users)
 }
 
-module.exports = {getAllUsers}
+// users post requist
+const postUsers = (req,res)=>{
+   let newUser = {
+    id : uuidv4(),
+    userName : req.body.userName,
+    email : req.body.email,
+   }
+   users.push(newUser)
+   res.status(200).json(newUser)
+}
+
+module.exports = {getAllUsers,postUsers}
--- routes/users.route.js
+++ routes/users.route.js
@@ -1,6 +1,7 @@
 const route = require("express").Router()
-const { getAllUsers } = require("../controllers/users.controller");
+const { getAllUsers,postUsers } = require("../controllers/users.controller");
 
 route.get("/",getAllUsers)
+route.post("/",postUsers)
 
 module.exports = route;
