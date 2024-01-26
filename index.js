f036e5acd20963fdd693deb5eba43c388a017740
SR Rony <146630569+SR-Rony@users.noreply.github.com>
2024-01-26 08:09:29 +0600

    mongodb is added

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 require("dotenv").config()
 let app = require("./app")
 let PORT =process.env.PORT || 500;
+const mongoose = require('mongoose');
 
 
-app.listen(PORT, ()=>{
+
+const connectedDB = async () =>{
+  try{
+    await mongoose.connect('mongodb://127.0.0.1:27017/test')
+    console.log('server is connected');
+  }catch(error){
+    console.log('data is not connected');
+    console.log(error);
+  }
+} 
+
+
+
+app.listen(PORT, async ()=>{
     console.log(`my server is runing at http://localhost:${PORT}`);
+    await connectedDB()
 })
